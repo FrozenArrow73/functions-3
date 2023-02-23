fbe69f806bf49645bcc3c8c897ab24ef7ca00363
Derek Wheeler <frozenarrow73@gmail.com>
2023-02-23 14:56:37 -0700

    arrayMethodsOne.js complete

--- arrayMethodsOne.js
+++ arrayMethodsOne.js
@@ -18,7 +18,8 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 // CODE HERE
-const evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+const evenNumbers  = mixedNumbers.filter((element, index, wholeArray)=>{ return element % 2 === 0})
+console.log(evenNumbers)
 
 
 
@@ -39,7 +40,8 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 // CODE HERE
-const postTaxPrices // = prices.map(/* Provide Your Callback Here );
+const postTaxPrices = prices.map( (element, index, wholeArray) => { return element * 1.07});
+console.log(postTaxPrices)
 
 
 
@@ -57,8 +59,8 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 // CODE HERE
-const totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
-
+const totalPopulation = populations.reduce((acc, cur) => {return acc + cur})
+console.log(totalPopulation)
 
 
 ////////// PROBLEM 4 //////////
@@ -82,8 +84,11 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 // CODE HERE
-const myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
-
+const myStrongest = monstersInYourPocket.filter((element, index, wholeArray) => {
+  
+  return element.CP > 200
+})
+console.log(myStrongest)
 
 
 ////////// PROBLEM 5 //////////
@@ -100,7 +105,8 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
 */
 
 // CODE HERE
-
+const totals = orders.map((element, index, wholeArray)=>{return element.price * (1 + element.tax)})
+console.log(totals)
 
 
 ////////// PROBLEM 6 //////////
@@ -120,3 +126,9 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
 */
 
 // CODE HERE
+bobsTotal = purchases.filter((element)=>{
+  return element.owner ==="Bob"
+}).reduce((acc, cur)=>{return acc + cur.price}, 0)
+
+
+console.log(bobsTotal)
